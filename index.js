c9d8f9c547043425260cdd78db7661dc2ba50e1e
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-10-29 00:55:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ $(function () {
     $('form').submit(function () {
 
         //練習エリア開始
-        var text1 = $('input[name="text1"] option:selected').val();
-        var select1 = $('input[name="select1"]').val();
+        var text1 = $('input[name="text1"]').val();
+        var select1 = $('input[name="select1"] option:selected').val();
         //練習エリア終了
         
         var date = $('input[name="date"]').val();
