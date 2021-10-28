9b88f5121ce589d48700c000a5d08efaa56a1745
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-10-28 22:01:36 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -26,7 +26,7 @@ function sendText(text) {
     if (!liff.isInClient()) {
         shareTargetPicker(text);
     } else {
-        //sendMessages("見積もり申し込み"); /////////文言「見積もり申し込み」の送信は成功したが、Gmailへの転送が成功しない。
+        sendMessages("見積もり申し込み"); /////////文言「見積もり申し込み」の送信は成功したが、Gmailへの転送が成功しない。
         //sendGmail();
         sendMessages(text);
     }
