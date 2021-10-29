2dc79a7c75c97cf3fa2510c51732718f99ffe012
sho-tanabe <93310029+sho-tanabe@users.noreply.github.com>
2021-10-30 02:23:51 +0900

    Update liff.js

--- liff.js
+++ liff.js
@@ -79,15 +79,15 @@ function sendautomail(text){
 //htmlファイルから移植した
  function() {
      window.addEventListener('load', function() { 
-                    var forms = document.getElementsByClassName('w-75 mx-auto');  
-                    var validation = Array.prototype.filter.call(forms,function(form) {
-                        form.addEventListener('submit', function(event) {
-                            if (form.checkValidity() === false) {
-                                event.preventDefault(); 
-                                event.stopPropagation(); 
-                            }
-                            form.classList.add('was-validated');
-                        }, false);
-                    });
-                }, false);
-            };
+         var forms = document.getElementsByClassName('w-75 mx-auto');  
+         var validation = Array.prototype.filter.call(forms,function(form) {
+             form.addEventListener('submit', function(event) {
+                 if (form.checkValidity() === false) {
+                     event.preventDefault(); 
+                     event.stopPropagation(); 
+                 }
+                 form.classList.add('was-validated');
+             }, false);
+         });
+     }, false);
+ };
